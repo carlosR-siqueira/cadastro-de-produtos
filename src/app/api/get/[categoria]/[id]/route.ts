ee0433df3db58aafc01e7512ce54c04591460032
Carlos Bekinho <carlos.dev.job@gmail.com>
2024-09-13 02:48:32 -0300

    feat(get): add get by 'categoria' and 'id'

---
+++ src/app/api/get/[categoria]/[id]/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse, NextRequest} from 'next/server';
+import { database } from '@lib/firebaseConfig';
+import { ref, get } from 'firebase/database';
+
+export async function GET(request: NextRequest, { params }: { params: { categoria: string, id: string } }) {
+    const {categoria, id } = params;
+    
+    try {
+      const dbRef = ref(database, `produto/${categoria}/${id}`);
+      const snapshot = await get(dbRef);
+      const data = snapshot.val();
+      
+  
+      if (data) {
+        return NextResponse.json(data);
+      } else {
+        return NextResponse.json({ message: 'Nenhum produto encontrado' }, { status: 404 });
+      }
+    } catch (error) {
+      return NextResponse.json({ message: 'Erro ao conectar com o banco de dados', error: (error as Error).message }, { status: 500 });
+    }
+  }
--- src/app/api/get/route.ts
+++ src/app/api/get/route.ts
@@ -1,13 +1,16 @@
 // src/app/api/produtos/get.ts
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest} from 'next/server';
 import { database } from '@lib/firebaseConfig';
 import { ref, get } from 'firebase/database';
 
 export async function GET() {
+  
+  
   try {
     const dbRef = ref(database, 'produto');
     const snapshot = await get(dbRef);
     const data = snapshot.val();
+    
 
     if (data) {
       return NextResponse.json(data);
@@ -18,3 +21,4 @@ export async function GET() {
     return NextResponse.json({ message: 'Erro ao conectar com o banco de dados', error: (error as Error).message }, { status: 500 });
   }
 }
+
